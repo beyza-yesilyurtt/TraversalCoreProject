904d49fbb502db136c1d10f794f75579ab262fb4
beyza-yesilyurtt <108866247+beyza-yesilyurtt@users.noreply.github.com>
2023-07-21 17:37:42 +0300

    second update

---
+++ ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SubAbout : ViewComponent
+    {
+        SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = subAboutManager.TGetList();
+            return View(values);
+        }
+    }
+}
