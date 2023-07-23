de2f857d0fdf24d2fa4f67e4881c2d99b447c4e3
beyza-yesilyurtt <108866247+beyza-yesilyurtt@users.noreply.github.com>
2023-07-23 19:04:27 +0300

    third update

---
+++ Controllers/DestinationController1.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController1 : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
+        }
+    }
+}
