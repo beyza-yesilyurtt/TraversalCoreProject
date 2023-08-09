e0a60aa43b75658a2495ac2af330ccdf7eb605c8
beyza-yesilyurtt <108866247+beyza-yesilyurtt@users.noreply.github.com>
2023-08-09 14:16:03 +0300

    seventh update

---
+++ Models/UserRegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace TraversalCoreProject.Models
+{
+	public class UserRegisterViewModel
+	{
+        [Required(ErrorMessage ="Lütfen isim giriniz.")]
+		public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen soyisim giriniz.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adı giriniz.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail adresinizi giriniz.")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        public string Password { get; set; }
+
+        [Compare("Password" , ErrorMessage ="Şifreler uyumlu değil.")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
