a9ac232bef9a145bf6a39ea95824d26ace4356f4
unknown <beyzayesilyurt2@gmail.com>
2023-08-18 22:58:57 +0300

    eighth commit

---
+++ Models/UserSignInViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+
+
+namespace TraversalCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adını giriniz.")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifreyi giriniz.")]
+        public string password { get; set; }
+    }
+}
